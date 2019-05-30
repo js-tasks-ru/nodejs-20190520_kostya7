38cd38fcbbffbb2c3dc44edd6df90c0ac4b271bd
kyazykov <kyazykov@mobiledimensiob.ru>
2019-05-30 16:53:26 +0300

    add solution on module 2 task 1

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,11 +4,22 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
   }
+
+  bufferArr = [];
 
   _transform(chunk, encoding, callback) {
+        this.bufferArr.push(chunk);
+        const buffer = Buffer.concat(this.bufferArr);
 
+       if (buffer.byteLength > this.limit) {
+         callback(new LimitExceededError);
+       } else {
+          callback(null, chunk);
+       }
   }
 }
+
 
 module.exports = LimitSizeStream;
---
+++ 2-module/1-task/index.js
@@ -0,0 +1,12 @@
+const LimitSizeStream = require('./LimitSizeStream');
+const fs = require('fs');
+
+const limitedStream = new LimitSizeStream({limit: 8}); // 8 байт
+const outStream = fs.createWriteStream('out.txt');
+limitedStream.pipe(outStream);
+
+limitedStream.write('hello');
+
+setTimeout(() => {
+    limitedStream.write('world');
+}, 10);
