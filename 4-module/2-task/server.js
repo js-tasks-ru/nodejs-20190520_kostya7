9ebc679981714f37a3840ce479dff3fb67a59ac5
kyazykov <kyazykov@mobiledimensiob.ru>
2019-06-03 10:24:56 +0300

    first solution on module 4 task 2

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,16 +1,59 @@
+// Не смог сделать стрим из тела запроса, чтобы потом в кастомный наш стрим прокинуть
+
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+// const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
+function ensureDirectoryExistence(filePath) {
+  const dirname = path.dirname(filePath);
+  fs.open(dirname, 'r', (err) => {
+    if (err !== null) {
+      ensureDirectoryExistence(dirname);
+      fs.mkdirSync(dirname);
+    }
+  });
+}
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'POST':
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('Dont support nested files');
+        return;
+      }
+
+      let body = '';
+
+      req.on('data', (chunk) => {
+        body += chunk.toString();
+
+        ensureDirectoryExistence(filepath);
+        if (body.length > 1e6) {
+          body = "";
+          res.writeHead(413, {'Content-Type': 'text/plain'}).end();
+          req.connection.destroy();
+        }
+      });
+
+
+      req.on('end', () => {
+        fs.stat(filepath, (err) => {
+          if (err !== null && body.length) {
+            res.statusCode = 201;
+            fs.writeFile(filepath, body, () => {});
+          } else {
+            res.statusCode = 409;
+          }
+          res.end(body);
+        });
+      });
 
       break;
 
@@ -21,3 +64,5 @@ server.on('request', (req, res) => {
 });
 
 module.exports = server;
+
+
--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,6 +12,25 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('Dont support nested files');
+      }
+
+      fs.unlink(filepath, err => {
+        if (err) {
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('File not found');
+          } else {
+            res.statusCode = 500;
+            res.end('Server error');
+          }
+        }
+
+        res.statusCode = 200;
+        res.end('File delete');
+      });
 
       break;
 
