c0aee4c3cad80630fe1172a2806d9cb4b74d1aea
kyazykov <kyazykov@mobiledimensiob.ru>
2019-06-12 14:34:59 +0300

    7-module 1-task

--- 6-module/2-task/app.js
+++ 6-module/2-task/app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const User = require('./models/User');
+const mongoose = require('mongoose');
 
 const app = new Koa();
 
@@ -36,21 +37,47 @@ const checkMailValidate = async (ctx, next) => {
   await next();
 };
 
+function validateId(ctx, next) {
+    const id = ctx.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        ctx.throw(400);
+    }
+
+    return next();
+}
+
+// async function handleMongooseValidationError(ctx, next) {
+//     try {
+//         await next();
+//     } catch(e) {
+//         if (e.name !== 'ValidationError') throw e;
+//
+//         ctx.status = 400;
+//         const errors = {};
+//
+//         for (const field in e.errors) {
+//             errors[field] = e.errors[field].message;
+//         }
+//
+//         ctx.body = {
+//             errors: errors
+//         };
+//     }
+// }
+
 router.get('/users', async (ctx) => {
-  ctx.body = await User.find({}).then((users) => users);
+  ctx.body = await User.find({});
 });
 
-router.get('/users/:id', async (ctx) => {
+router.get('/users/:id', validateId, async (ctx) => {
   const id = ctx.params.id;
-  ctx.body = await User.findById(id)
-      .then((res) => {
-    if (res === null) ctx.throw(404);
+  const user = await User.findById(id);
+      if (!user) {
+          ctx.throw(404);
+      }
 
-    return res;
-  })
-      .catch((e) => {
-        e.name === 'NotFoundError' ? ctx.throw(404) : ctx.throw(400);
-      });
+      ctx.body = user;
 });
 
 router.patch('/users/:id', checkMailValidate, async (ctx) => {
@@ -71,6 +98,22 @@ router.patch('/users/:id', checkMailValidate, async (ctx) => {
       });
 });
 
+// router.patch('/users/:id', validateId, handleValidationErrors, async (ctx) => {
+//     const fields = _.pick(ctx.request.body, ['displayName', 'email']);
+//
+//     const user = await User.findByIdAndUpdate(ctx.params.id, fields, {
+//         runValidators: true,
+//         new: true,
+//     });
+//
+//     ctx.body = user;
+// });
+
+// router.post('/users', handleMongooseValidationError, async (ctx) => {
+//     const fields = _.pick(ctx.request.body, ['displayName', 'email']);
+//     ctx.body = await User.create(fields);
+// });
+
 router.post('/users', async (ctx) => {
   const body = ctx.request.body;
   const user = new User({
@@ -86,15 +129,15 @@ router.post('/users', async (ctx) => {
       });
 });
 
-router.delete('/users/:id', async (ctx) => {
+router.delete('/users/:id', validateId, async (ctx) => {
   const id = ctx.params.id;
 
-  ctx.body = await User.findByIdAndDelete(id).then(result => {
-    if (result === null) {
+  const user = await User.findByIdAndDelete(id);
+    if (!user) {
       ctx.throw(404);
     }
-    return result;
-  });
+
+    ctx.body = user;
 });
 
 app.use(router.routes());
